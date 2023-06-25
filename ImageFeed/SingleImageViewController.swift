8c633d4cb4f464a65a0042cd0ea31d68af8652e4
PLDDev <wildmagicactive@gmail.com>
2023-06-25 22:49:29 +0300

    no message

--- ImageFeed/SingleImageViewController.swift
+++ ImageFeed/SingleImageViewController.swift
@@ -55,8 +55,5 @@ extension SingleImageViewController: UIScrollViewDelegate {
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         imageView
     }
-    func scrollViewDidZoom(_ scrollView: UIScrollView) {
-        imageView.center = self.scrollView.center
-    }
 }
 
